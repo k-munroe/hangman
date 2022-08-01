472d263915d790f06689126588469fa3a924fac9
Kamanzi Rama-Munroe <kmunroe@mailfence.com>
2022-07-31 21:30:52 -0400

    replace correct guessed letter with underscore

--- hangman.py
+++ hangman.py
@@ -1,21 +1,29 @@
 import random
-#Step 1 
-
 word_list = ["aardvark", "baboon", "camel"]
-
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
-
 chosen_word = random.choice(word_list)
 
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
 
-guess = input("Please guess a letter:\n").lower()
+#TODO-1: - Create an empty List called display.
+#For each letter in the chosen_word, add a "_" to 'display'.
+#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
+display = []
+word_length = len(chosen_word)
+for _ in range(word_length):
+    display += "_"
 
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+guess = input("Guess a letter: ").lower()
 
-for letters in chosen_word:
-    
-    if letters == guess:
-        print(f"The letter is {letters}. You selected {guess}. You guessed right!")
-    else:
-        print(f"The letter is {letters}. You selected {guess}. You guessed incorrectly :(")
+#TODO-2: - Loop through each position in the chosen_word;
+#If the letter at that position matches 'guess' then reveal that letter in the display at that position.
+#e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
+for position in range(word_length):
+    letter = chosen_word[position]
+    #print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+    if letter == guess:
+        display[position] = letter
+
+#TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
+#Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
+print(display)
