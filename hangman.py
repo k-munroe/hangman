714d4b4d868c5e4856cbcdafaae42b943da74810
Kamanzi Rama-Munroe <kmunroe@mailfence.com>
2022-08-02 23:10:34 -0400

    thought of new todo so I can loop back and continue submitting guesses

--- hangman.py
+++ hangman.py
@@ -11,6 +11,12 @@ word_length = len(chosen_word)
 for _ in range(word_length):
     display += "_"
 
+
+#TODO-1: - Use a while loop to let the user guess again. 
+# The loop should only stop once the user has guessed all 
+# the letters in the chosen_word and 'display' has no more blanks ("_"). 
+# Then you can tell the user they've won.    
+
 guess = input("Guess a letter: ").lower()
 
 
