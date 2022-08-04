f4e88bd82f779e6d483433c31e6679076193321a
Kamanzi Rama-Munroe <kmunroe@mailfence.com>
2022-08-03 22:46:40 -0400

    add ascii art

--- hangman.py
+++ hangman.py
@@ -1,4 +1,62 @@
 import random
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 
@@ -12,10 +70,7 @@ for _ in range(word_length):
     display += "_"
 
 
-#TODO-1: - Use a while loop to let the user guess again. 
-# The loop should only stop once the user has guessed all 
-# the letters in the chosen_word and 'display' has no more blanks ("_"). 
-# Then you can tell the user they've won.    
+ 
 
 while chosen_word != ''.join(display):
 
